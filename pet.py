6a4eb072f83c68c37c52cef82d369c83f96f38be
mumbua-mutinda <berlynmumbua@gmail.com>
2025-04-17 16:05:21 +0300

    Implemented hunger and energy

--- pet.py
+++ pet.py
@@ -7,6 +7,21 @@ class Pet:
         self.tricks = []
 
     # Let each group member add their methods here
+    #Reduces hunger by 3, but never lets it go below 0.
+    #increases happiness by 1 but doesn't go beyond 10
+    def eat(self):
+     if self.hunger >= 3:
+        self.hunger -= 3
+     else:
+         self.hunger = 0
+     self.happiness = min(self.happiness + 1, 10)
+     print(f"{self.name} has eaten. Hunger is now {self.hunger}, Happiness is {self.happiness}.")
+    #adds energy by 5 but doesn't go beyond 10
+    def sleep(self):
+     self.energy = min(self.energy + 5, 10)
+     print(f"{self.name} has slept. Energy is now {self.energy}.")
+
+
 
     def play(self):
         # Decrease energy and increase happiness
@@ -17,6 +32,7 @@ class Pet:
             print("You played with your pet.")
         else:
             print(f"{self.name} is too tired to play.")
+        
         
 
     #This will be used to print the status as required
